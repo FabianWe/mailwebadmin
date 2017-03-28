4c4b1b43fb9b31aa772e3261645a5929a305d2dc
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2017-03-28 15:51:38 +0200

    design changes

--- static/default/default_theme.js
+++ static/default/default_theme.js
@@ -44,3 +44,32 @@ function post_login() {
      $('#login-status').addClass('alert-danger').removeClass('alert-info').html("Authentication error, username / password wrong.");
   });
 }
+
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });
+});
